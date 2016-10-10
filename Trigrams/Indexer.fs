bed9323c86c7a77eacd754e65c9e6d494885f754
Hackle Wayne <hacklew@hotmail.com>
2016-10-11 02:38:30 +1300

    Add Node type

--- Trigrams/Indexer.fs
+++ Trigrams/Indexer.fs
@@ -3,11 +3,32 @@
 module Indexer =
     open System
 
+    type Node<'a> = { First: 'a; Second: 'a; Third: 'a list }
+
     let index (elements: 'a list) =
 
         let rec index' remaining indexed =
             match remaining with
-            | x1::x2::x3::xs -> index' (x2::x3::xs) ((x1, x2, x3) :: indexed)
+            | x1::x2::x3::xs -> index' (x2::x3::xs) (((x1, x2), x3) :: indexed)
             | _ -> indexed
 
         index' elements []
+        |> List.groupBy (fun e -> e |> fst)
+        |> List.map (fun g -> { First = g |> fst |> fst;
+                                Second =  g |> fst |> snd;
+                                Third = g |> snd |> List.map snd })
+
+    let private rand' = new System.Random()
+    let pickRandom list =
+        rand'.Next(0, list |> List.length) 
+        |> (fun i -> List.item i list)
+
+    let rec random words firstTwo =
+        seq {
+            let w' = words |> List.tryFind (fun w -> w |> fst = firstTwo)
+            if w'.IsSome then 
+                let found = w'.Value |> snd
+                yield found |> pickRandom
+                for next in random words (firstTwo |> snd, found) do
+                    yield next
+        }
--- Trigrams/IndexerTests.fs
+++ Trigrams/IndexerTests.fs
@@ -2,15 +2,28 @@
 
 module IndexerTests =
     open Xunit
+    open Indexer
 
     [<Fact>]
     let ``Converts list to trigrams`` () =
         let input = [ 1 .. 4 ]
         let expected = 
             [
-                (2, 3, 4);
-                (1, 2, 3)
+                { First = 2; Second = 3; Third = [ 4 ] };
+                { First = 1; Second = 2; Third = [ 3 ] }
             ]
         let actual = Indexer.index input
 
-        Assert.Equal<(int * int * int) list>(expected, actual)
+        Assert.Equal<Node<int> list>(expected, actual)
+
+    [<Fact>]
+    let ``Converts list to trigrams, allowing dups for same keys`` () =
+        let input = [ 1; 2; 1; 2; 3 ]
+        let expected = 
+            [
+                { First = 1; Second = 2; Third = [ 3; 1 ] };   
+                { First = 2; Second = 1; Third = [ 2 ] }
+            ]
+        let actual = Indexer.index input
+
+        Assert.Equal<Node<int> list>(expected, actual)
