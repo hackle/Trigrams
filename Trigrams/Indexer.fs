2b11ba90413d63a6454b59833ff6a618cdf48947
Hackle Wayne <hacklew@hotmail.com>
2016-10-10 19:18:30 +1300

    Can make trigrams from list

---
+++ Trigrams/Indexer.fs
@@ -0,0 +1,13 @@
+﻿namespace Trigrams
+
+module Indexer =
+    open System
+
+    let index (elements: 'a list) =
+
+        let rec index' remaining indexed =
+            match remaining with
+            | x1::x2::x3::xs -> index' (x2::x3::xs) ((x1, x2, x3) :: indexed)
+            | _ -> indexed
+
+        index' elements []
---
+++ Trigrams/IndexerTests.fs
@@ -0,0 +1,16 @@
+﻿namespace Trigrams
+
+module IndexerTests =
+    open Xunit
+
+    [<Fact>]
+    let ``Converts list to trigrams`` () =
+        let input = [ 1 .. 4 ]
+        let expected = 
+            [
+                (2, 3, 4);
+                (1, 2, 3)
+            ]
+        let actual = Indexer.index input
+
+        Assert.Equal<(int * int * int) list>(expected, actual)
